0df76d60c45496b51c505233773ace878f5e9585
scarletmgc <scarletmgc@gmail.com>
2024-02-29 21:14:52 -0400

    funcion copiar lista

--- javasc/javascript.js
+++ javasc/javascript.js
@@ -13,37 +13,33 @@ let texto = [[/enter/g,"e"],[/ai/g,"a"],[/ofer/g,"o"],[/ufat/g,"u"],[/imes/g,"i"
 function captarDatos(){
     document.querySelector(".mostrar").classList.add("ocultar");
     let entradaDeDatos=document.querySelector(".entradaDeDatos").value;
+    console.log(entradaDeDatos)
     
     for (let index = 0; index < vocales.length; index++) {
         entradaDeDatos = entradaDeDatos.replaceAll(vocales[index][0],vocales[index][1]);
     }
-    document.querySelector(".salidaDeDatos").innerText = entradaDeDatos;
+    document.querySelector(".textoACopiar").innerText=entradaDeDatos;
     
     console.log(entradaDeDatos);
     document.querySelector(".entradaDeDatos").value="";
 }
-/*
-//class de la salida de datos
+
+//class de la salida de datos desencriptar
 function devolverDatos(){
     let salidaDeDatos= document.querySelector(".salidaDeDatos").value;
     for (let index = 0; index < texto.length; index++) {
         salidaDeDatos = salidaDeDatos.replaceAll(texto[index][1],texto[index][0]);        
     }
-    
-    //document.querySelector(".salidaDeDatos").appendChild(salidaDeDatos);
     document.querySelector(".mostrar").classList.add("ocultar");
     console.log(salidaDeDatos);
 }
 
 //class de copiar datos
 function copiar(){
-    salidaDeDatos= document.querySelector(".salidaDeDatos").value;
-    salidaDeDatos.select();
-    salidaDeDatos.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(salidaDeDatos.value);
+    let salidaDeDatos= document.querySelector(".textoACopiar").innerText;
+    console.log(salidaDeDatos);
+    
+  navigator.clipboard.writeText(salidaDeDatos);
   
-  // Alert the copied text
-  alert("Copied the text: " + salidaDeDatos.value);
-}*/
+  document.querySelector(".entradaDeDatos").value=salidaD
+}
