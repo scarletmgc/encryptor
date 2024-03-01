43445d18d52c7fe4da6c8193c77bdde935f823fa
scarletmgc <scarletmgc@gmail.com>
2024-03-01 14:46:49 -0400

    media query 768

--- javasc/javascript.js
+++ javasc/javascript.js
@@ -5,12 +5,12 @@
     // const letraU = /u/g;
 
 let vocales = [[/e/g,"enter"],[/a/g,"ai"],[/o/g,"ofer"],[/u/g,"ufat"],[/i/g,"imes"]];
-let texto = [[/enter/g,"e"],[/ai/g,"a"],[/ofer/g,"o"],[/ufat/g,"u"],[/imes/g,"i"]];
+//let texto = [[/enter/g,"e"],[/ai/g,"a"],[/ofer/g,"o"],[/ufat/g,"u"],[/imes/g,"i"]];
 
 
 //class de captar datos
 
-function captarDatos(){
+function encriptardor(){
     document.querySelector(".mostrar").classList.add("ocultar");
     let entradaDeDatos=document.querySelector(".entradaDeDatos").value;
     console.log(entradaDeDatos)
@@ -26,12 +26,15 @@ function captarDatos(){
 
 //class de la salida de datos desencriptar
 function devolverDatos(){
-    let salidaDeDatos= document.querySelector(".salidaDeDatos").value;
-    for (let index = 0; index < texto.length; index++) {
-        salidaDeDatos = salidaDeDatos.replaceAll(texto[index][1],texto[index][0]);        
+    let entradaDeDatos=document.querySelector(".entradaDeDatos").value;
+    for (let jindex = 0; jindex < vocales.length; jindex++) {
+        entradaDeDatos = entradaDeDatos.replaceAll(vocales[jindex][1],vocales[jindex][0]);        
     }
     document.querySelector(".mostrar").classList.add("ocultar");
-    console.log(salidaDeDatos);
+    console.log(entradaDeDatos);
+
+    document.querySelector(".textoACopiar").innerText=entradaDeDatos;
+
 }
 
 //class de copiar datos
